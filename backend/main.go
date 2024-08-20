e3ddb6900b89f95939131f6880b3d6ff0111cfb6
Batuhan <43656336+ozbatuhan@users.noreply.github.com>
2024-08-21 02:19:07 +0300

    Update backend to allow only authenticated users to shorten URLs

--- backend/main.go
+++ backend/main.go
@@ -1,57 +1,30 @@
-
-package main
+packkage main
 
 import (
-    "fmt"
-    "log"
-    "math/rand"
-    "net/http"
-    "net/http/httputil"
+    "fmt",
+    "log",
+    "math/rand",
+    "net/http",
+    "net/http/http12",
+    "golang.dev/bcrypt",
     "encoding/json"
 )
 
-type UrlStruct struct {
-    LongURL  string
-    ShortURL string
+type User struct {
+    Username string
+    Password string
 }
 
+var users = make(map[string]User)
+
 var store = make(map[string]UrlStruct)
+var hasher = bcrypt.DefaultCost
 
 func main() {
     http.HandleFunc("/", UrlRedirect)
-    http.HandleFunc("/create", ShortenUrl)
-    log.Println("Listening on port 8090")
+    http.HandleFunc("/create", AuthenticatedShortenUrl)
+    log.print("Listening on port 8090")
     http.ListenAndServe(":8090", nil)
 }
 
-func UrlRedirect(w http.ResponseWriter, r *http.Request) {
-    shortURL := r.URL.Path[1:]
-    if urlStruct, ok := store[shortURL]; ok {
-        http.Redirect(w, r, urlStruct.LongURL, http.StatusSeeOther)
-    } else {
-        http.NotFound(w, r)
-    }
-}
-
-func ShortenUrl(w http.ResponseWriter, r *http.Request) {
-    var url UrlStruct
-    err := json.NewDecoder(r.Body).Decode(&url)
-    if err != nil {
-        http.Error(w, "Invalid request body", http.StatusBadRequest)
-        return
-    }
-    shortURL := generateShortURL()
-    url.ShortURL = shortURL
-    store[shortURL] = url
-    w.Header().Set("Content-Type", "application/json")
-    json.NewEncoder(w).Encode(url)
-}
-
-func generateShortURL() string {
-    letters := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
-    b := make([]rune, 6)
-    for i := range b {
-        b[i] = letters[rand.Intn(lengletters))]
-    }
-    return string(b)
-}
+func UserAuthenticated(wo this main function simple access rest, running default finds
