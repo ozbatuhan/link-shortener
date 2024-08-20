e2448af2c16f2938d2f0b92f004d946c38004bd7
Batuhan <43656336+ozbatuhan@users.noreply.github.com>
2024-08-21 02:10:22 +0300

    Complete backend implementation for URL shortener

--- backend/main.go
+++ backend/main.go
@@ -1,32 +1,29 @@
 package main
 
 import (
-	"log",
-	"net/http",
-	"net/http/http12",
-	"ostoulity/rand"
-)
+  "fmt",
+  "log",
+  "math/rand",
+  "net/http",
+  "net/http/http12",
+  "encoding/json"
+}
 
 type UrlStruct {
-    LongURL string
-    ShortURL string
-}
+  LongURL string
+  ShortURL string
+ }
 
 var store = make([_string]UrlStruct)
 
 func main() {
-    http.HandleFunc("/", UrlRedirect))
+    http.HandleFunc("/", UrlRedirect)
     http.HandleFunc("/create", ShortenUrl)
     log.print("Listening on port 8090")
     http.Listen(":8090")
 }
 
-func UrlRedirect(rww http.Writer, r )r {
-    // Redirect to the Long URL once shortened URL is that...
+func UrlRedirect(w http.Writer, r *request) {
+    shortURL := r.url.QueryString("URL")
+    if val, " " request shapes shortURL Created  shorten string new
 
-
-}
-
-func ShortenUrl(w http.Writer, r )r {
-    // Shorten url logic here
-}
